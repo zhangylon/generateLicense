77f125f389a4ef9fcb231e61e03d23498b7d78d4
zhangyl <zhangyl@vecentek.com>
2025-10-30 14:58:26 +0800

    fix:增加测试项id

--- src/main/java/com/vecentek/licenseGenerate/generator/LicenseGeneratorTool.java
+++ src/main/java/com/vecentek/licenseGenerate/generator/LicenseGeneratorTool.java
@@ -42,7 +42,7 @@ public class LicenseGeneratorTool {
             38L, 39L, 40L, 41L, 42L, 43L, 44L, 45L, 46L, 47L, 48L, 49L, 50L, 51L, 52L, 53L, 54L, 55L, 56L, 57L, 58L, 59L, 60L, 61L,
             62L, 63L, 64L, 65L, 66L, 67L, 68L, 69L, 70L, 71L, 72L, 73L, 74L, 75L, 76L, 77L, 78L, 79L, 80L, 81L, 82L, 83L, 84L, 85L,
             86L, 87L, 88L, 89L, 90L, 91L, 92L, 93L, 94L, 95L, 96L, 97L, 98L, 99L, 100L, 101L, 102L, 103L, 104L, 105L, 106L, 107L,
-            108L, 109L, 110L, 111L, 112L, 113L,114L,115L};
+            108L, 109L, 110L, 111L, 112L, 113L,114L,115L,116L, 117L, 118L, 119L, 120L, 121L, 122L, 103807441762961L, 119418510061697L};
 
     private static String[] allowedModules = new String[]{"/task", "/task/list", "/task/create", "/useCase", "/result", "/system"};
 
